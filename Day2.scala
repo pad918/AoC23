bc41f399dc1a781a6a28be9feda79e2f822ca581
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2023-12-08 23:23:41 +0100

    Day 2 refactor

--- Day2.scala
+++ Day2.scala
@@ -8,39 +8,9 @@ object Day2 {
 
   val colors = List("red", "green", "blue")
 
-  def run1() : Unit = {
+  def parseGames() : List[(Int, Array[Map[String, Int]])] = {
     val source = Source.fromFile("E:\\programmering\\Scala\\adventofcode\\AoC23\\src\\main\\scala\\day2_1.txt")
-
-    //(id, Map[])
-    val games = source.getLines().map(
-      l =>
-      {
-        val sideSplit = l.split(":")
-        val id = sideSplit.head.split(" ").last.toInt
-        val sets = sideSplit.last.split(";")
-        val maps = sets.map(
-          s => {
-            val colorPairs = s.split(", ")
-            colorPairs.map(cp => {
-              val parts = cp.split(" ").filter(s => s.trim().nonEmpty)
-              (parts.last, parts.head.toInt)
-            }
-            ).toMap
-          }
-        )
-        (id, maps)
-      }
-    ).toList
-
-    val sum = games.filter(m => !m._2.toList.exists(p => p.exists((s, i) => maxValues(s)<i))).map(g => g._1).sum
-    println(s"Total sum = $sum");
-  }
-
-  def run2() : Unit = {
-    val source = Source.fromFile("E:\\programmering\\Scala\\adventofcode\\AoC23\\src\\main\\scala\\day2_1.txt")
-
-    //(id, Map[])
-    val games = source.getLines().map(
+    source.getLines().map(
       l => {
         val sideSplit = l.split(":")
         val id = sideSplit.head.split(" ").last.toInt
@@ -58,16 +28,24 @@ object Day2 {
         (id, maps)
       }
     ).toList
+  }
+
+  def run1() : Unit = {
+    val games = parseGames()
+    val sum = games.filter(m => !m._2.toList.exists(p => p.exists((s, i) => maxValues(s)<i))).map(g => g._1).sum
+    println(s"Total sum = $sum");
+  }
+
+  def run2() : Unit = {
+    val games = parseGames()
 
     val minimums = games.map(
       g => {
-        var min = colors.map(c => (c, 0)).toMap
         g._2.reduce((b, a) => colors.map(c => (c, Math.max(a.getOrElse(c, 1), b.getOrElse(c, 1)))).toMap)
       }
     )
 
-    val sum = minimums.map(m => m.toList.map(v => v._2).reduce((a, b) => a*b)).sum
-
+    val sum = minimums.map(m => m.toList.map(v => v._2).product).sum
     println(s"Total sum 2 = $sum");
   }
 
